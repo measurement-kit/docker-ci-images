5f6e90703b15d404d8f456e535659bca29f1cf56
Simone Basso <bassosimone@gmail.com>
2018-10-20 11:39:07 +0200

    Start making appveyor.bat more flexible

--- script/appveyor.bat
+++ script/appveyor.bat
@@ -1,3 +1,14 @@
-cmake "-G%CMAKE_GENERATOR%" .
-cmake --build . -- /nologo /property:Configuration=Release
-ctest --output-on-failure -C Release -a
+@echo off
+if exist CMakeLists.txt (
+  if [%1] == [] (
+    echo Usage: %0 "cmake-generator-name"
+    exit /b 1
+  )
+  cmake "-G%~1"
+  if %errorlevel% neq 0 exit /b %errorlevel%
+  cmake --build . -- /nologo /property:Configuration=Release
+  if %errorlevel% neq 0 exit /b %errorlevel%
+  ctest --output-on-failure -C Release -a
+  if %errorlevel% neq 0 exit /b %errorlevel%
+  exit /b 1
+)
